d07a6d036dd375c2fe92d3fb016df59e5fe06846
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-09-03 17:48:37 +0300

    feat: add weather api data mapping

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,6 +1,50 @@
 import type { Location } from "@/types/locations";
 import type { TempUnit } from "@/types/tempUnit";
 
+export type WeatherLocation = {
+  name: string;
+  timezone: string;
+};
+
+export type WeatherCurrent = {
+  weatherCode: number;
+  isDay: boolean;
+  temp: number;
+  tempUnit: string;
+  apparentTemp: number;
+  maxTemp: number;
+  minTemp: number;
+  sunrise: string;
+  sunset: string;
+  rainChance: number;
+  humidity: number;
+  cloudCover: number;
+  uvIndex: number;
+  windSpeed: number;
+  surfacePressure: number;
+};
+
+export type WeatherHourly = {
+  dateTime: string;
+  temp: number;
+  rainChance: number;
+};
+
+export type WeatherDaily = {
+  date: string;
+  weatherCode: number;
+  maxTemp: number;
+  minTemp: number;
+  rainChance: number;
+};
+
+export type Weather = {
+  location: WeatherLocation;
+  current: WeatherCurrent;
+  hourly: WeatherHourly[];
+  daily: WeatherDaily[];
+};
+
 type WeatherResponse = {
   timezone: string;
 
@@ -41,7 +85,7 @@ type WeatherResponse = {
 
 const baseUrl = "https://api.open-meteo.com/v1/forecast";
 
-export async function fetchWeather(location: Location, tempUnit: TempUnit) {
+export async function fetchWeather(location: Location, tempUnit: TempUnit): Promise<Weather> {
   const params = new URLSearchParams();
 
   params.set("latitude", String(location.lat));
@@ -71,4 +115,48 @@ export async function fetchWeather(location: Location, tempUnit: TempUnit) {
   }
 
   const data: WeatherResponse = await response.json();
+
+  const { timezone, current_units, current, hourly, daily } = data;
+
+  const localTime = new Date(current.time);
+  const startIndex = hourly.time.findIndex(time => new Date(time) > localTime);
+
+  return {
+    location: {
+      name: location.name,
+      timezone: timezone
+    },
+
+    current: {
+      weatherCode: current.weather_code,
+      isDay: current.is_day === 1,
+      temp: current.temperature_2m,
+      tempUnit: current_units.temperature_2m,
+      apparentTemp: current.apparent_temperature,
+      maxTemp: daily.temperature_2m_max[0],
+      minTemp: daily.temperature_2m_min[0],
+      sunrise: daily.sunrise[0],
+      sunset: daily.sunset[0],
+      rainChance: current.precipitation_probability,
+      humidity: current.relative_humidity_2m,
+      cloudCover: current.cloud_cover,
+      uvIndex: current.uv_index,
+      windSpeed: current.wind_speed_10m,
+      surfacePressure: current.surface_pressure
+    },
+
+    hourly: Array.from({ length: 7 }, (_, i) => ({
+      dateTime: hourly.time[startIndex + i * 2],
+      temp: hourly.temperature_2m[startIndex + i * 2],
+      rainChance: hourly.precipitation_probability[startIndex + i * 2]
+    })),
+
+    daily: Array.from({ length: 7 }, (_, i) => ({
+      date: daily.time[i],
+      weatherCode: daily.weather_code[i],
+      maxTemp: daily.temperature_2m_max[i],
+      minTemp: daily.temperature_2m_min[i],
+      rainChance: daily.precipitation_probability_max[i]
+    }))
+  };
 }
