d1fc4969d0d3fe5239d29533a6ce3b4ffd4f17fe
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-09-06 09:22:53 +0300

    feat: add weather location

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,5 +1,7 @@
 import type { Weather } from "@/api/weather";
 
+import WeatherLocation from "./WeatherLocation/WeatherLocation";
+
 type WeatherProps = {
   weather: Weather;
   updateWeather: () => void;
@@ -8,6 +10,7 @@ type WeatherProps = {
 export default function Weather({ weather, updateWeather }: WeatherProps) {
   return (
     <div className="weather">
+      <WeatherLocation location={weather.location} updateWeather={updateWeather} />
     </div>
   );
 }
