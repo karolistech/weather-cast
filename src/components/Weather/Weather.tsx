3d201f282c2167fb944812b92c8dbdda19033797
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-09-16 10:15:54 +0300

    feat: add weather hourly

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -2,6 +2,7 @@ import type { Weather } from "@/types/weather";
 
 import WeatherLocation from "./WeatherLocation/WeatherLocation";
 import WeatherCurrent from "./WeatherCurrent/WeatherCurrent";
+import WeatherHourly from "./WeatherHourly/WeatherHourly";
 
 type WeatherProps = {
   weather: Weather;
@@ -13,6 +14,7 @@ export default function Weather({ weather, updateWeather }: WeatherProps) {
     <div className="weather">
       <WeatherLocation location={weather.location} updateWeather={updateWeather} />
       <WeatherCurrent current={weather.current} />
+      <WeatherHourly hourly={weather.hourly} />
     </div>
   );
 }
