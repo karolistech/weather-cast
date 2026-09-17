d5badb455cf4f33f6a09c338d79d598868cce0a6
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-09-17 06:46:51 +0300

    feat: add weather daily

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -3,6 +3,7 @@ import type { Weather } from "@/types/weather";
 import WeatherLocation from "./WeatherLocation/WeatherLocation";
 import WeatherCurrent from "./WeatherCurrent/WeatherCurrent";
 import WeatherHourly from "./WeatherHourly/WeatherHourly";
+import WeatherDaily from "./WeatherDaily/WeatherDaily";
 
 type WeatherProps = {
   weather: Weather;
@@ -15,6 +16,7 @@ export default function Weather({ weather, updateWeather }: WeatherProps) {
       <WeatherLocation location={weather.location} updateWeather={updateWeather} />
       <WeatherCurrent current={weather.current} />
       <WeatherHourly hourly={weather.hourly} />
+      <WeatherDaily daily={weather.daily} />
     </div>
   );
 }
