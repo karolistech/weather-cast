7ba8e021c034f029fbf0c35a950110d476d50d09
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-09-22 19:05:51 +0300

    feat: add settings menu styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import SearchBar from "./SearchBar/SearchBar";
 import SettingsMenu from "./SettingsMenu/SettingsMenu";
 
+import "./Header.css";
+
 export default function Header() {
   return (
     <div className="header">
